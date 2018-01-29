cd33d40f3f39e11bca12b058cc5b4e6c70fa8e18
xenopol <x.enopol@yahoo.com>
2018-01-29 16:37:45 +0100

    Add Yarn, babel-stage-2 and React.Fragment

--- client/components/App.js
+++ client/components/App.js
@@ -1,12 +1,12 @@
-import React from 'react'
+import React, { Component, Fragment } from 'react'
 
-class App extends React.Component {
+class App extends Component {
   render () {
     return (
-      <div>
+      <Fragment>
         <h1>React Boilerplate</h1>
         <h2>Using react-hot-loader</h2>
-      </div>
+      </Fragment>
     )
   }
 }
